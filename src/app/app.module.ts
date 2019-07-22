32e876a780c0c5c68d34703f416042773b6f0412
Marina Cardozo <maru.cardozo02@gmail.com>
2019-07-22 17:22:31 -0300

    modificacion del app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import {
-  EventsListComponent,
-  EventThumbnailComponent,
-  EventService,
-  DurationPipe,
-  EventDetailsComponent,
-  CreateEventComponent,
-  EventRouteActivator,
-  EventListResolver,
-  CreateSesionComponent,
-  SessionListComponent
-} from './events/index';
-
-import { EventsAppComponent } from './events-app.component';
-import { NavBarComponent } from './nav/navbar.component';
-import { JQ_TOKEN, TOASTR_TOKEN, Toastr, CollapsibleWellComponent } from './common/index';
-import { RouterModule } from '@angular/router';
-import { appRoutes } from './routes';
-import { Error404Component } from './error/404.component';
-import { AuthService } from './user/auth.service';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { CollapsibleWellComponent, JQ_TOKEN, ModalTriggerDirective, SimpleModalComponent, Toastr, TOASTR_TOKEN } from './common/index';
+import { Error404Component } from './error/404.component';
+import { EventsAppComponent } from './events-app.component';
+import { CreateEventComponent, CreateSesionComponent, DurationPipe, EventDetailsComponent, EventListResolver, EventRouteActivator, EventService, EventsListComponent, EventThumbnailComponent, SessionListComponent } from './events/index';
+import { NavBarComponent } from './nav/navbar.component';
+import { appRoutes } from './routes';
+import { AuthService } from './user/auth.service';
+
+
 
 //declare let toastr: Toastr;
 let toastr: Toastr = window['toastr'];
@@ -31,36 +20,39 @@ let jQuery = window['$'];
 // los servicios van en providers
 //Imports es para la importacion de otros modules
 @NgModule({
-  declarations: [
-    EventsAppComponent,
-    EventsListComponent,
-    EventThumbnailComponent,
-    NavBarComponent,
-    EventDetailsComponent,
-    CreateEventComponent,
-    Error404Component,
-    CreateSesionComponent,
-    SessionListComponent,
-    CollapsibleWellComponent,
-    DurationPipe
-  ],
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), FormsModule, ReactiveFormsModule],
-  providers: [
-    EventService,
-    { provide: TOASTR_TOKEN, useValue: toastr },
-    EventRouteActivator,
-    EventListResolver,
-    AuthService,
-    { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
-  ],
-  bootstrap: [EventsAppComponent]
+    declarations: [
+        EventsAppComponent,
+        EventsListComponent,
+        EventThumbnailComponent,
+        NavBarComponent,
+        EventDetailsComponent,
+        CreateEventComponent,
+        Error404Component,
+        CreateSesionComponent,
+        SessionListComponent,
+        CollapsibleWellComponent,
+        SimpleModalComponent,
+        ModalTriggerDirective,
+        DurationPipe
+    ],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes), FormsModule, ReactiveFormsModule],
+    providers: [
+        EventService,
+        { provide: TOASTR_TOKEN, useValue: toastr },
+        { provide: JQ_TOKEN, useValue: jQuery },
+        EventRouteActivator,
+        EventListResolver,
+        AuthService,
+        { provide: 'canDeactivateCreateEvent', useValue: checkDirtyState }
+    ],
+    bootstrap: [EventsAppComponent]
 })
-export class AppModule {}
+export class AppModule { }
 
 export function checkDirtyState(component: CreateEventComponent) {
-  if (component.isDirty) {
-    return window.confirm('Esta seguro de que se quiere ir? aun no ha salvado el evento');
-  } else {
-    return true;
-  }
+    if (component.isDirty) {
+        return window.confirm('Esta seguro de que se quiere ir? aun no ha salvado el evento');
+    } else {
+        return true;
+    }
 }
