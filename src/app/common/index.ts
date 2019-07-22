8b63a6638c51e10bdde584436cd2002f72ec96b2
Marina Cardozo <maru.cardozo02@gmail.com>
2019-07-22 17:24:33 -0300

    Nuevos archivos y modificaciones del index

--- src/app/common/index.ts
+++ src/app/common/index.ts
@@ -1,3 +1,6 @@
-export * from './jQuery.service';
-export * from './toastr.service';
 export * from './collapsible-well.component';
+export * from './jQuery.service';
+export * from './modalTrigger.directive';
+export * from './simpleModal.component';
+export * from './toastr.service';
+
