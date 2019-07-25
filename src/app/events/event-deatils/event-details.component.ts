6cae07dcebbc308ff279f52321a9883303295a19
Marina Cardozo <maru.cardozo02@gmail.com>
2019-07-25 10:24:16 -0300

    modificaciones

--- src/app/events/event-deatils/event-details.component.ts
+++ src/app/events/event-deatils/event-details.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { EventService } from '../shared/event.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { IEvent, ISession } from '../shared';
+import { EventService } from '../shared/event.service';
 
 @Component({
-  templateUrl: './event-details.component.html',
-  styles: [
-    `
+    templateUrl: './event-details.component.html',
+    styles: [
+        `
       .container {
         padding-left: 20px;
         padding-right: 20px;
@@ -18,31 +18,36 @@ import { IEvent, ISession } from '../shared';
         cursor: pointer;
       }
     `
-  ]
+    ]
 })
 export class EventDetailsComponent implements OnInit {
-  event: IEvent;
-  addMode: boolean;
-  filterBy: string = 'all';
-  sortBy: string = 'votes';
-  constructor(private eventService: EventService, private route: ActivatedRoute) {}
-  ngOnInit() {
-    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
-  }
+    event: IEvent;
+    addMode: boolean;
+    filterBy: string = 'all';
+    sortBy: string = 'votes';
+    constructor(private eventService: EventService, private route: ActivatedRoute) { }
+    ngOnInit() {
+        this.route.params.forEach((params: Params) => {
+            this.event = this.eventService.getEvent(+params['id']);
+        })
+        this.addMode = false;
+        //Anterior forma de ruteo
+        // this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+    }
 
-  addSession() {
-    this.addMode = true;
-  }
+    addSession() {
+        this.addMode = true;
+    }
 
-  saveNewSession(session: ISession) {
-    const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
-    session.id = nextId + 1;
-    this.event.sessions.push(session);
-    this.eventService.updateEvent(this.event);
-    this.addMode = false;
-  }
+    saveNewSession(session: ISession) {
+        const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
+        session.id = nextId + 1;
+        this.event.sessions.push(session);
+        this.eventService.updateEvent(this.event);
+        this.addMode = false;
+    }
 
-  cancelAddSession() {
-    this.addMode = false;
-  }
+    cancelAddSession() {
+        this.addMode = false;
+    }
 }
